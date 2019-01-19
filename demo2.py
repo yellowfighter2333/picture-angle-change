fc0afff0110656a30769862f94bed968d48d594d
zhangqingyan <648953887@qq.com>
2019-01-19 22:09:00 +0800

    the version is complete but it does not work well

---
+++ demo2.py
@@ -0,0 +1,52 @@
+from transform import four_point_transformation
+import numpy as np
+import cv2
+import argparse#this pck is used in commmand line argument 
+import imutils
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--image",required = True,
+    help = "path to the image to be required")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+ratio = image.shape[0]/500.0
+orig = image.copy()
+image = imutils.resize(image,height=500)
+#convert the image to grayscale, blur it ,and find edges
+
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray,(5,5),0)
+edged = cv2.Canny(gray,75,200)
+
+#show the original image and the edge detected image
+print("edge detection")
+cv2.imshow("Image",image)
+cv2.imshow("Edged",edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cnts = cv2.findContours(edged.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts,key = cv2.contourArea,reverse = True)[:5]
+
+for c in cnts:
+    peri = cv2.arcLength(c,True)
+    approx = cv2.approxPolyDP(c,0.02*peri,True)
+
+
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+ 
+print("find coutours of paper")
+cv2.drawContours(image,[screenCnt],-1,(0.255,0),2)
+cv2.imshow("Outline",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+warped = four_point_transformation(orig,screenCnt.reshape(4,2)*ratio)
+
+cv2.imshow("Original",imutils.resize(orig,height=650))
+cv2.imshow("Scanned ",imutils.resize(warped,height = 650))
+cv2.waitKey(0)
